0947f748930cd6ae9a78c206669d09558e80582c
kabinet303 <kabinet303@localhost.localdomain>
2025-09-26 10:10:58 +0300

    Базовая структура сайта

---
+++ Moy_Ne_Sam/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>мой не сам</title>
+    <link rel='icon' href='images/logo.jpeg'>
+    <link rel='stylesheet' href='css/style.css'>
+</head>
+<body>
+    <header> <img src='images/logo.jpeg' alt='логотип'>
+        <h1>мой не сам</h1>
+    </header>
+
+    <nav>
+        <a href="/demoekzamen/Moy_Ne_Sam">Главная</a>
+        <a href="/demoekzamen/Moy_Ne_Sam/admin">Админ-панель</a>
+    </nav>
+
+    <main>
+    </main>
+
+    <script src="js/script.js"></script>
+    
+    <footer>
+        2025
+    <footer>
+</body>
+</html>
